f1de1b312c3696395ba2e474563bd6380d784501
MohabEldemery <121578039+MohabEldemery@users.noreply.github.com>
2025-10-16 15:47:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from whisper.utils import get_writer
 import warnings
 warnings.filterwarnings("ignore", message=".*FP16 is not supported on CPU.*")
 
-# Load Whisper model.
+# Load Whisper model
 model = whisper.load_model("base")   # you can also try "small", "medium" Or or "large" Or "turbo" but RAM Usage & Processing time will increase
 
 # Input video and output folder & Run transcription
@@ -49,6 +49,7 @@ print("Saved SRT in", output_dir)
 #     f.write(final_summary)
 #
 # print("Saved Summary TXT in", summary_path)
+
 
 
 
